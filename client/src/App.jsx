641c96ca84365243cf3839fddb12962f72ae4201
nardo88 <nardo1988@mail.ru>
2021-12-16 21:40:29 +0300

    1

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import './App.css';
 
@@ -10,12 +10,39 @@ function App() {
   const [input, setInput] = useState([])
   // функция отправки сообщения
   const sendMessage = async () => {
+    // отправляем POST запрос к нашему API и отправляем объект сообщения
     await axios.post('http://localhost:5000/new-messages', {
       message: input,
       id: Date.now()
-  })
+    })
   }
 
+  
+  useEffect(() => {
+    subscribe()
+  }, [])
+
+  // создаем функцию подписку
+  const subscribe = async () => {
+    // в блоке try мы отправляем GET запрос
+    try{
+      // и когда ответ придет, мы запишем его в переменную data с помощью 
+      // диструктуризации
+      const {data} = await axios.get('http://localhost:5000/get-message')
+      // полученные данные записываем в state 
+      // здесь обязательно записываем через callback
+      setMessages(prev => [data, ...prev])
+      // после чего снова возобновляем подписку
+      await subscribe()
+    } catch(e){
+      // в блок catch мы момпадаем когда по истечении долгого времени мы так
+      // ничего не получили и срок запроса истек
+      setTimeout(() => {
+         //здесь через 0.5 сек возобновляем подписку
+        subscribe()
+      }, 500)
+    }
+  }
   return (
     <div className="App">
       <div className="container">
@@ -24,9 +51,9 @@ function App() {
           <button className="send" onClick={sendMessage}>send</button>
         </div>
         <div className="chat">
-          {messages.map(item => 
-              <div className="message" key={item.id}>{item.message}</div>
-            )}
+          {messages.map(item =>
+            <div className="message" key={item.id}>{item.message}</div>
+          )}
         </div>
       </div>
     </div>
--- server/index.js
+++ server/index.js
@@ -1,16 +1,17 @@
 // импортируем библиотеки
-const express = require('express');
-const cors = require('cors')
-const events = require('events')
+import express from 'express'
+import cors from 'cors'
+import events from 'events'
+
 // указываем на каком порту будем разворачивать сервер
 const PORT = 5000
-// создаем imitter lka работы с событиями
+// создаем imitter для работы с событиями
 const emitter = new events.EventEmitter();
 // слоздаем экземпляр express
 const app = express()
-// передаем cors
+// передаем cors что бы отправлять запросы в браузере
 app.use(cors())
-
+// прописываем JSON что бы в response можно было отправлять данные
 app.use(express.json())
 // // создаем эндпоинты для отправки и получения сообщений
 app.get('/get-message', (req, res) => {
@@ -23,14 +24,15 @@ app.get('/get-message', (req, res) => {
     
 })
 app.post('/new-messages', ((req, res) => {
+    // получаем сообщение из тела запроса
     const message = req.body;
+    // создаем событие и передаем в него сообщение
     emitter.emit('newMessage', message)
+    // возращаем статус 200
     res.json({
         status: 200
     })
 }))
-
-
 // запускаем express
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
 
